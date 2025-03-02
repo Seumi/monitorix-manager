43110efebc44df61fc46f5866ea54c8f8b06e854
Seumi <seumi@qq.com>
2025-03-02 22:21:51 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,10 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
+
+
+
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
